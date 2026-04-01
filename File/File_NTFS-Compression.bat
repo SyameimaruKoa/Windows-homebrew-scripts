1ae6e1a4f5ab220cddc7de1e01f0e1ed6186ff2c
SyameimaruKoa-Q736 <toho.komeizi.aya@gmail.com>
2026-04-01 12:44:00 +0900

    特定拡張子除外機能を追加してみた

--- File/File_NTFS-Compression.bat
+++ File/File_NTFS-Compression.bat
@@ -10,13 +10,13 @@ chcp 932 > nul
 
 :: 引数があるかチェック
 if "%~1"=="" (
-  echo 対象のファイルまたはフォルダをドラッグアンドドロップしてください。
-  pause
-  exit /b
+    echo 対象のファイルまたはフォルダをドラッグアンドドロップしてほしいのじゃ。
+    pause
+    exit /b
 )
 
 :: 圧縮モードか解除モードかを選択
-echo 圧縮モードまたは解除モードを選択してください:
+echo 圧縮モードまたは解除モードを選択するのじゃ:
 echo [1] 圧縮
 echo [2] 圧縮解除
 choice /C 12 /N /M "選択 (1-2): "
@@ -27,7 +27,7 @@ goto END
 
 :COMPRESS
 :: 圧縮レベルの選択
-echo 圧縮レベルを選択してください:
+echo 圧縮レベルを選択するのじゃ:
 echo [1] 最速 (XPRESS4K)
 echo [2] 標準 (XPRESS8K)
 echo [3] 最大 (XPRESS16K)
@@ -41,58 +41,100 @@ if %errorlevel%==3 set "option=/EXE:XPRESS16K"
 if %errorlevel%==4 set "option=/EXE:LZX"
 
 :: 強制圧縮モードの確認
-choice /C YN /N /M "強制圧縮モードを使用しますか？ (Y/N): "
+choice /C YN /N /M "強制圧縮モードを使用するか？ (Y/N): "
 set "force_mode=0"
 if %errorlevel%==1 set "force_mode=1"
 
+:: 除外拡張子の入力
+echo.
+echo 除外したい拡張子があればスペース区切りで入力するのじゃ。（例: .zip .mp4 .jpg）
+echo 特に除外しない場合は、何も入力せずにEnterを押すがよい。
+set "exclude_exts="
+set /P "exclude_exts=> "
+
 :: 圧縮処理
 echo.
-if %force_mode%==1 (
-  echo 強制圧縮中（%option% /F）...
-  for %%G in (%*) do (
-    echo  - "%%~G"
-    if exist "%%~G\" (
-      rem フォルダの場合
-      compact /C %option% /F /S:"%%~G"
-      ) else (
-      rem ファイルの場合
-      compact /C %option% /F "%%~G"
+if "!exclude_exts!"=="" (
+    if %force_mode%==1 (
+        echo 強制圧縮中（%option% /F）...
+        for %%G in (%*) do (
+            echo  - "%%~G"
+            if exist "%%~G\" (
+                rem フォルダの場合
+                compact /C %option% /F /S:"%%~G"
+            ) else (
+                rem ファイルの場合
+                compact /C %option% /F "%%~G"
+            )
+        )
+    ) else (
+        echo 圧縮中（%option%）...
+        for %%G in (%*) do (
+            echo  - "%%~G"
+            if exist "%%~G\" (
+                rem フォルダの場合
+                compact /C %option% /S:"%%~G"
+            ) else (
+                rem ファイルの場合
+                compact /C %option% "%%~G"
+            )
+        )
     )
-  )
-  ) else (
-  echo 圧縮中（%option%）...
-  for %%G in (%*) do (
-    echo  - "%%~G"
-    if exist "%%~G\" (
-      rem フォルダの場合
-      compact /C %option% /S:"%%~G"
-      ) else (
-      rem ファイルの場合
-      compact /C %option% "%%~G"
+) else (
+    set "f_opt="
+    if !force_mode!==1 set "f_opt=/F"
+    
+    echo 除外拡張子「!exclude_exts!」を適用して圧縮中（!option! !f_opt!）なのじゃ...
+    for %%G in (%*) do (
+        if exist "%%~G\" (
+            rem フォルダの場合
+            echo  - フォルダを解析中: "%%~G"
+            for /f "delims=" %%F in ('dir /b /s /a:-d "%%~G"') do (
+                set "skip=0"
+                for %%E in (!exclude_exts!) do (
+                    if /i "%%~xF"=="%%E" set "skip=1"
+                )
+                if "!skip!"=="0" (
+                    compact /C !option! !f_opt! "%%F" >nul
+                ) else (
+                    echo    [スキップ] "%%F"
+                )
+            )
+        ) else (
+            rem ファイルの場合
+            set "skip=0"
+            for %%E in (!exclude_exts!) do (
+                if /i "%%~xG"=="%%E" set "skip=1"
+            )
+            if "!skip!"=="0" (
+                compact /C !option! !f_opt! "%%~G"
+            ) else (
+                echo  - [スキップ] "%%~G"
+            )
+        )
     )
-  )
 )
 goto END
 
 :DECOMPRESS
 :: 圧縮解除処理
 echo.
-echo 圧縮解除中...
+echo 圧縮解除中なのじゃ...
 for %%G in (%*) do (
-  echo  - "%%~G"
-  if exist "%%~G\" (
-    rem フォルダの場合
-    compact /U /S:"%%~G"
+    echo  - "%%~G"
+    if exist "%%~G\" (
+        rem フォルダの場合
+        compact /U /S:"%%~G"
     ) else (
-    rem ファイルの場合
-    compact /U "%%~G"
-  )
+        rem ファイルの場合
+        compact /U "%%~G"
+    )
 )
 goto END
 
 :END
 echo.
-echo 全ての処理が完了しました。
+echo 全ての処理が完了したのじゃ。
 endlocal
 pause
 exit /b
@@ -100,15 +142,16 @@ exit /b
 :show_help
 echo.
 echo [概要]
-echo  NTFS 圧縮/圧縮解除をファイルやフォルダに対して実行します。
+echo  NTFS 圧縮/圧縮解除をファイルやフォルダに対して実行するのじゃ。
+echo  特定の拡張子を除外して圧縮することもできるぞ。
 echo.
 echo [使い方]
 echo  %~nx0 ^<file_or_folder1^> ^<file_or_folder2^> ...
-echo  対話で 圧縮/解除、圧縮レベル、強制の有無 を選択します。
+echo  対話で 圧縮/解除、圧縮レベル、強制の有無、除外拡張子 を選択するのじゃ。
 echo.
 echo [注意]
-echo  システムファイルや既に圧縮されたファイルには効果が限定的です。
+echo  システムファイルや既に圧縮されたファイルには効果が限定的じゃぞ。
 echo.
-echo 何かキーを押すと閉じます...
+echo 何かキーを押すと閉じるのじゃ...
 pause
 exit /b
